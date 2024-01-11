df5355daadba20ea5f838c60f96b86ddcb9fc11a
Udara Dananjaya Aththanayaka <udarapaymedia@gmail.com>
2024-01-11 10:35:19 +0530

    Update UserController insert Index Function

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    //
+    public function index() {
+        try {
+            $users = User::all();
+            
+            $responseData = [
+                'success' => true,
+                'data' => [
+                    'message' => 'Data successfully retrieved',
+                    'users' => $users,
+                ],
+            ];
+
+            return response()->json($responseData, 200);
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+            return response()->json(['error' => 'An unexpected error occurred'], 500);
+        }
+    }
+    
+    
+
 }
