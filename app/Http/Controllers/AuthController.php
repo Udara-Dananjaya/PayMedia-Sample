9d2c7868124fbbf05cff8515c13f69cb7232c824
Udara Dananjaya Aththanayaka <udarapaymedia@gmail.com>
2024-01-11 10:14:54 +0530

    Update AuthController With JWT and Create Login and Logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends Controller
 {
-    //
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+        if (!$token = JWTAuth::attempt($credentials)) {
+            return response()->json(['error' => 'Invalid credentials'], 401);
+        }
+        return response()->json(['token' => $token,'status'=>true]);
+    }
+
+    public function checkToken(){
+        return response()->json(['success'=>true],200);
+    }
+
+    public function logout(){
+        $logout =auth()->logout();
+        return response()->json(['success'=>true],200);
+    }
 }
