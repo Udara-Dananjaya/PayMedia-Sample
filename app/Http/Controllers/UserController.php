6f2db27c723eb90a0c662df37ea058241e475d04
Udara Dananjaya Aththanayaka <udarapaymedia@gmail.com>
2024-01-11 11:21:45 +0530

    Update User Controller With CURD

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,13 +7,16 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
+
 
 class UserController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         try {
             $users = User::all();
-            
+
             $responseData = [
                 'success' => true,
                 'data' => [
@@ -28,7 +31,105 @@ class UserController extends Controller
             return response()->json(['error' => 'An unexpected error occurred'], 500);
         }
     }
-    
-    
 
+    public function create(Request $request)
+    {
+        try {
+            $request->validate([
+                'name' => 'required|string|max:255',
+                'email' => 'required|email|unique:users',
+                'password' => 'required|string|min:8|max:12|regex:/^.*(?=.{3,})(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[\d\x])(?=.*[!$#%]).*$/',
+                'img' => 'image|mimes:jpeg,png,jpg,gif|max:2048',
+            ]);
+
+            $user = new User();
+
+            $user->name = Str::ucfirst($request->input('name'));
+            $user->email = $request->input('email');
+            $user->password = Hash::make($request->input('password'));
+
+            if ($request->hasFile('img')) {
+                $image = $request->file('img');
+                $imageName = 'user_' . $user->id . '.' . $image->getClientOriginalExtension();
+                $image->move(public_path('images'), $imageName);
+                $user->img = 'images/' . $imageName;
+            }
+
+            $user->save();
+
+            return response()->json(['success' => true, 'data' => ['user' => $user]], 201);
+        } catch (ValidationException $e) {
+            return response()->json(['success' => false, 'error' => $e->errors()], 422);
+        } catch (\Exception $e) {
+            return response()->json(['success' => false, 'error' => $e->getMessage()], 500);
+        }
+    }
+
+    public function update(Request $request, $id)
+    {
+        try {
+            $request->validate([
+                'name' => 'string|max:255',
+                'email' => 'email|unique:users,email,' . $id,
+                'password' => 'nullable|string|min:8|max:12|regex:/^.*(?=.{3,})(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[\d\x])(?=.*[!$#%]).*$/',
+                'img' => 'image|mimes:jpeg,png,jpg,gif|max:2048',
+            ]);
+
+            $user = User::findOrFail($id);
+
+            if ($request->filled('name') && $request->input('name') !== $user->name) {
+                $user->name = Str::ucfirst($request->input('name'));
+            }
+
+            if ($request->filled('email') && $request->input('email') !== $user->email) {
+                $user->email = $request->input('email');
+            }
+
+            if ($request->filled('password')) {
+                $user->password = Hash::make($request->input('password'));
+            }
+
+            if ($request->hasFile('img')) {
+                $image = $request->file('img');
+                $imageName = 'user_' . $user->id . '.' . $image->getClientOriginalExtension();
+                $image->move(public_path('images'), $imageName);
+                $user->img = 'images/' . $imageName;
+            }
+
+            $user->save();
+
+            return response()->json(['success' => true, 'data' => ['user' => $user]], 200);
+        } catch (ValidationException $e) {
+            return response()->json(['success' => false, 'error' => $e->errors()], 422);
+        } catch (\Exception $e) {
+            return response()->json(['success' => false, 'error' => $e->getMessage()], 500);
+        }
+    }
+
+    public function show($id)
+    {
+        try {
+            $user = User::find($id);
+
+            if ($user) {
+                return response()->json(['success' => true, 'data' => ['user' => $user]], 200);
+            } else {
+                return response()->json(['success' => false, 'error' => 'User not found'], 404);
+            }
+        } catch (\Exception $e) {
+            return response()->json(['success' => false, 'error' => $e->getMessage()], 500);
+        }
+    }
+
+    public function delete($id)
+    {
+        try {
+            $user = User::findOrFail($id);
+            $user->delete();
+
+            return response()->json(['success' => true, 'data' => ['message' => 'User deleted successfully']], 200);
+        } catch (\Exception $e) {
+            return response()->json(['success' => false, 'error' => $e->getMessage()], 500);
+        }
+    }
 }
