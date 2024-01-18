dbe613182bc42a5f0ccd8b336b9ee52b19dfb38c
Udara Dananjaya <dev.dananjaya@gmail.com>
2024-01-18 14:55:19 +0530

    Create A new Router

--- src/router/index.js
+++ src/router/index.js
@@ -11,12 +11,17 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
+    path: '/login',
+    name: 'Login',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/LoginView.vue')
+  },
+  {
+    path: '/',
+    name: 'home',
+    component: () => import(/* webpackChunkName: "about" */ '../views/ListView.vue')
   }
 ]
 
