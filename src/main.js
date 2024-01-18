ec4a1c42d72ab412e2c052316e268d7386c5dbe8
Udara Dananjaya <dev.dananjaya@gmail.com>
2024-01-18 13:20:26 +0530

    Add comments to Main.js

--- src/main.js
+++ src/main.js
@@ -1,17 +1,28 @@
+// Importing Vue library and required components and plugins
 import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import Buefy from 'buefy'
-import 'buefy/dist/buefy.css'
+import App from './App.vue' // Importing the main App component
+import router from './router' // Importing Vue Router for navigation
+import store from './store' // Importing Vuex store for state management
+import Buefy from 'buefy' // Importing Buefy for UI components
+import 'buefy/dist/buefy.css' // Importing Buefy styles
+import { ValidationObserver, ValidationProvider } from 'vee-validate'; // Importing VeeValidate for form validation
 
+// Importing custom CSS files
+import './css/styles.css'
 
+// Configuring Vue to not show production tip in the console
 Vue.config.productionTip = false
 
+// Using Buefy as a Vue plugin
 Vue.use(Buefy)
 
+// Registering VeeValidate components globally
+Vue.component("ValidationProvider", ValidationProvider);
+Vue.component("ValidationObserver", ValidationObserver);
+
+// Creating a new Vue instance
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+  router, // Using the imported router for navigation
+  store, // Using the imported Vuex store for state management
+  render: h => h(App) // Rendering the main App component
+}).$mount('#app') // Mounting the Vue instance to the '#app' element in the HTML
