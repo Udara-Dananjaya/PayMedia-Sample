15e77e00db40f2becfc8f44a16b7a358886620cb
Udara Dananjaya <dev.dananjaya@gmail.com>
2024-01-22 23:45:05 +0530

    Adding Vue Pinia

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue'
 import router from './router'
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
+import { createPinia, PiniaVuePlugin } from 'pinia'
+import VueCompositionAPI from '@vue/composition-api'
+
+Vue.use(PiniaVuePlugin)
+const pinia = createPinia()
+Vue.use(VueCompositionAPI)
 
 Vue.use(Buefy)
 
@@ -10,5 +16,6 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
+  pinia,
   render: h => h(App)
 }).$mount('#app')
