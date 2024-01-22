87820559248a8f82513927ab9e898e5148e15b47
Udara Dananjaya <dev.dananjaya@gmail.com>
2024-01-22 22:23:04 +0530

    Final

--- src/appSettings.js
+++
@@ -1,8 +0,0 @@
-let timeOutDurationMins = 30;
-
-const appSettings = {
-  $api_url:process.env.VUE_APP_API_BASE_URL,
-  timeoutDuration: timeOutDurationMins * 60 * 1000,
-};
-
-export { appSettings };
--- src/helpers/validators/validator.js
+++
@@ -1,6 +0,0 @@
-import { extend } from 'vee-validate';
-import * as rules from 'vee-validate/dist/rules';
-
-Object.keys(rules).forEach(rule => {
-  extend(rule, rules[rule]);
-});
