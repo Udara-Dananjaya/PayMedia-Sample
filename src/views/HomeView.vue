e0d36d8e9b6440af2709bcc68612ccbf052baec3
Udara Dananjaya <dev.dananjaya@gmail.com>
2024-01-23 17:00:05 +0530

    Remove Lines in HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,66 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TableComp :columns="columns" :data="users" @edit-user="handleEditUser" @delete-user="handleDeleteUser"></TableComp>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import TableComp from '@/components/TableComp.vue';
+import { BuefyHelper } from '@/helpers/buefyHelper';
+
+import NetworkManager from "@/network";
 
 export default {
-  name: 'HomeView',
+  data() {
+    return {
+      users: [],
+      columns: [
+        { field: "id", label: "ID", width: "40", numeric: true, searchable: true },
+        { field: "name", label: "Name", searchable: true },
+        { field: "email", label: "Email", searchable: true },
+        { field: "img", label: "Image" },
+        { field: "action", label: "Action", centered: true, sortable: false },
+      ]
+    };
+  },
   components: {
-    HelloWorld
+    TableComp
+  },
+  mounted() {
+    this.fetchUserData();
+  },
+  methods: {
+    async fetchUserData() {
+      try {
+        const response = await NetworkManager.apiRequest("list");
+        console.log(response);
+        this.users = response.data.users.map(user => ({
+          ...user,
+          img: user.img ? `${process.env.VUE_APP_BASE_URL}${user.img}` : null,
+        }));
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    }, handleEditUser(id) {
+      console.log('Edit user with ID:', id);
+    },
+    async handleDeleteUser(id) {
+      const isConfirm = await BuefyHelper.showConfirmAlert('Confirm Action', 'Are you sure you want to proceed?', 'Yes');
+      if (isConfirm) {
+        try {
+          await NetworkManager.apiRequest(
+            `delete/${id}`,
+            {},
+            true,
+            "application/json"
+          );
+          this.fetchUserData();
+        } catch (error) {
+          console.error("Error deleting user:", error);
+        }
+      }
+
+    }
   }
-}
+};
 </script>
