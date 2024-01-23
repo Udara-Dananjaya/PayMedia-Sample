3dbe79331cabdf50d8008b2f94207bab853135ca
Udara Dananjaya <dev.dananjaya@gmail.com>
2024-01-23 10:33:14 +0530

    Update Store index

--- src/store/index.js
+++ src/store/index.js
@@ -4,25 +4,17 @@ import auth from './auth.js';
 
 Vue.use(Vuex)
 
-// export default new Vuex.Store({
-//   state: {
-//   },
-//   getters: {
-//   },
-//   mutations: {
-//   },
-//   actions: {
-//   },
-//   modules: {
-//     auth
-//   }
-// })
+export default new Vuex.Store({
+	state: {
+	},
+	getters: {
+	},
+	mutations: {
+	},
+	actions: {
+	},
+	modules: {
+		auth
+	}
+})
 
-
-const store = new Vuex.Store({
-  modules: {
-    auth
-  }
-});
-
-export default store;
