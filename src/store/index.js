6ccde70f0bfc3f7fa8c06479305b9740c6c7995c
Udara Dananjaya <dev.dananjaya@gmail.com>
2024-01-23 10:32:47 +0530

    Add Auth Module to store

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,28 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
+import auth from './auth.js';
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+// export default new Vuex.Store({
+//   state: {
+//   },
+//   getters: {
+//   },
+//   mutations: {
+//   },
+//   actions: {
+//   },
+//   modules: {
+//     auth
+//   }
+// })
+
+
+const store = new Vuex.Store({
   modules: {
+    auth
   }
-})
+});
+
+export default store;
