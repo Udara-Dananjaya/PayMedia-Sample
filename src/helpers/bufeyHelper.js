17c04935dd50ca944496c640cffe0ca4abc9a727
Udara Dananjaya <dev.dananjaya@gmail.com>
2024-01-23 09:59:32 +0530

    Create Buefy Helpers

---
+++ src/helpers/bufeyHelper.js
@@ -0,0 +1,48 @@
+import { SnackbarProgrammatic as Snackbar } from 'buefy'
+import { ToastProgrammatic as Toast } from 'buefy'
+import { DialogProgrammatic as Dialog } from 'buefy'
+
+const BuefyHelper = {
+    showSnackBar(message, actionText, type, callback) {
+        Snackbar.open({
+            message: message,
+            type: type,
+            position: 'is-top',
+            actionText: actionText,
+            indefinite: true,
+            onAction: callback
+        })
+    },
+    showToastMessage(message, type, duration = 5000) {
+        Toast.open({
+            duration: duration,
+            message: message,
+            type: type,
+        })
+    },
+    showAlert(title, message,confirmText='OK') {
+        Dialog.alert({
+            title: title,
+            message: message,
+            confirmText: confirmText,
+            type: 'is-danger',
+            hasIcon: true,
+            icon: 'times-circle',
+            iconPack: 'fa',
+            ariaRole: 'alertdialog',
+            ariaModal: true
+        });
+    },
+    showConfirmAlert(title, message, confirmText) {
+        Dialog.confirm({
+            title: title,
+            message: message,
+            confirmText: confirmText,
+            type: 'is-danger',
+            hasIcon: true,
+            onConfirm: () =>this.showToastMessage('Account deleted!')
+        })
+    },
+}
+
+export { BuefyHelper }
