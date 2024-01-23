ce369719ea5b82786962a157cd4f24d4dd910783
Udara Dananjaya <dev.dananjaya@gmail.com>
2024-01-23 10:30:18 +0530

    Create and Setup Auth.js

---
+++ src/store/auth.js
@@ -0,0 +1,55 @@
+// store/index.js
+import NetworkManager from "@/network";
+import router from '@/router';
+
+const state = {
+    authToken: localStorage.getItem('authToken') || null,
+};
+
+const getters = {
+    authToken: (state) => state.authToken,
+};
+
+const mutations = {
+    SET_AUTH_TOKEN(state, token) {
+        state.authToken = token;
+        localStorage.setItem('authToken', token);
+    },
+};
+
+const actions = {
+    setAuthToken({ commit }, token) {
+        commit('SET_AUTH_TOKEN', token);
+        console.log(token);
+    },
+    async checkAuthStatus() {
+        try {
+            const authToken = localStorage.getItem('authToken');
+            if (!authToken) {
+                router.push('/login');
+                return Promise.reject('No authToken available');
+            }
+            const response = await NetworkManager.apiRequest('checkToken', 'application/json', '', true);
+            console.log('Response:', response);
+            if (response.data && response.data.success) {
+                return Promise.resolve();
+            } else {
+                if (response.data && response.data.message === 'Unauthenticated.') {
+                    router.push('/login');
+                }
+                return Promise.reject('Authentication check failed');
+            }
+        } catch (error) {
+            console.error('Error checking auth status:', error);
+            return Promise.reject(error);
+        }
+    },
+};
+
+export default {
+    namespaced: true,
+    state,
+    getters,
+    mutations,
+    actions,
+};
