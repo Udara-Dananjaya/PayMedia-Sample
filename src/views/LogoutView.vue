d1d933484f4554cccb8e9fa6bcb0da4d6cbf0b0d
Udara Dananjaya <dev.dananjaya@gmail.com>
2024-01-24 10:58:12 +0530

    Make Logout

---
+++ src/views/LogoutView.vue
@@ -0,0 +1,25 @@
+<template>
+	<div></div>
+  </template>
+  
+  <script>
+  import NetworkManager from "@/network";
+  
+  export default {
+	async mounted() {
+		await this.logOutUser();
+	},
+	methods: {
+		async logOutUser() {
+			try {
+				await NetworkManager.apiRequest('logout');
+				this.$store.dispatch('auth/delAuthToken'); // Pass null or an empty string if needed
+				this.$router.push('/login');
+			} catch (error) {
+				console.error('Error during logout:', error);
+			}
+		},
+	},
+  };
+  </script>
+  
